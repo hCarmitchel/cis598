766bd17f08a7fd49d34e442d751a15b3179b073f
Heather Carmitchel <heathercarmitchel@gmail.com>
2013-04-25 22:16:44 -0500

    File path

--- config/initializers/clock.rb
+++ config/initializers/clock.rb
@@ -1,8 +1,8 @@
-require File.expand_path('../../config/boot',        __FILE__)
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../../boot',        __FILE__)
+require File.expand_path('../../environment', __FILE__)
 require 'clockwork'
 
 include Clockwork
 
-every(4.minutes, 'Queueing interval job') { Delayed::Job.enqueue IntervalJob.new }
-every(1.day, 'Queueing scheduled job', :at => '14:17') { Delayed::Job.enqueue ScheduledJob.new }
+every(4.minutes, 'Queueing interval job') { Delayed::Job.enqueue TvShow.parseIMDB }
+every(1.day, 'Queueing scheduled job', :at => '14:17') { Delayed::Job.enqueue TvShow.parseIMDB }
