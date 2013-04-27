8e1a6e577979c0e48b4af99a7dffe963c0a1983c
Heather Carmitchel <heathercarmitchel@gmail.com>
2013-04-26 23:06:29 -0500

    changing path

---
+++ config/initializers/clock.rb
@@ -0,0 +1,9 @@
+require_relative 'boot'
+require_relative 'environment'
+
+require 'clockwork'
+
+include Clockwork
+
+every(4.minutes, 'Queueing interval job') { Delayed::Job.enqueue TvShow.parseIMDB }
+every(1.day, 'Queueing scheduled job', :at => '14:17') { Delayed::Job.enqueue TvShow.parseIMDB }
