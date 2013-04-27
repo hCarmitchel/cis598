873ca92cc8babeacebb2f60506c1f7304fd55eac
Heather Carmitchel <heathercarmitchel@gmail.com>
2013-04-26 23:41:50 -0500

    path

--- config/initializers/clock.rb
+++ config/initializers/clock.rb
@@ -1,6 +1,5 @@
-require_relative '../../boot.rb'
-require_relative '../../environment.rb'
-
+require File.expand_path('../../config/boot', __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
 require 'clockwork'
 
 include Clockwork
