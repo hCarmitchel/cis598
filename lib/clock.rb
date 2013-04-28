f48b56ecab387c8c4cf21f467e450c28f459e2dc
Heather Carmitchel <heathercarmitchel@gmail.com>
2013-04-28 12:59:10 -0500

    Updating clock schedule

--- lib/clock.rb
+++ lib/clock.rb
@@ -8,5 +8,5 @@ Clockwork.configure do |config|
   config[:tz] = 'America/Indiana/Knox'
 end
 
-every(1.day, 'Parse TV shows', :at => '12:45') { Delayed::Job.enqueue TvShow.parseIMDB }
+every(1.day, 'Parse TV shows', :at => '13:05') { Delayed::Job.enqueue TvShow.parseIMDB }
 every(7.days, 'Weekly parse TV shows', :at => '01:11') { Delayed::Job.enqueue TvShow.parseIMDB }
